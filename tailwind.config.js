6da1b08596b76b80db510ea360ca4bd46a020c39
quendalyngenie <117054157+quendalyngenie@users.noreply.github.com>
2026-04-08 22:29:47 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+    colors:{
+      cblue: '#3F60A0',
+      dblue: '#1B3267',
+      grey: '#F5F5F5'
+    }
+  },
+  plugins: [],
+}
